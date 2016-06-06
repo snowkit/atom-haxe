c6fe90eeb625546809c66349bbdb7be4b85b0164
jeremyfa <contact@jeremyfa.com>
2016-06-06 19:25:56 +0200

    Remove optional empty arg

--- src/utils/Run.hx
+++ src/utils/Run.hx
@@ -8,7 +8,7 @@ class Run {
 
     public static function haxe(args:Array<String>, ?options:ExecOptions, ?ondataout:String->Void, ?ondataerr:String->Void):Promise<ExecResult> {
 
-        var cmd:String = atom.config.get('haxe.haxe_path', {});
+        var cmd:String = atom.config.get('haxe.haxe_path');
         if (cmd == null || cmd.length == 0) cmd = 'haxe';
 
         return Exec.run(cmd, args, options, ondataout, ondataerr);
@@ -17,7 +17,7 @@ class Run {
 
     public static function haxelib(args:Array<String>, ?options:ExecOptions, ?ondataout:String->Void, ?ondataerr:String->Void):Promise<ExecResult> {
 
-        var cmd:String = atom.config.get('haxe.haxelib_path', {});
+        var cmd:String = atom.config.get('haxe.haxelib_path');
         if (cmd == null || cmd.length == 0) cmd = 'haxelib';
 
         return Exec.run(cmd, args, options, ondataout, ondataerr);
